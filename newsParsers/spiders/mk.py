e8ef60eca480abb70704d24f89c08f7af573d25f
vasilewvv <vasilewvv@gmail.com>
2021-01-27 20:03:34 +0300

    scrap first news page

--- newsParsers/spiders/mk.py
+++ newsParsers/spiders/mk.py
@@ -7,4 +7,16 @@ class MkSpider(scrapy.Spider):
     start_urls = ['http://mk.ru/']
 
     def parse(self, response):
-        pass
+        for news in response.css('li.news-listing__item'):
+            try:
+                yield {
+                    'title': news.css('h3::text').get().strip(),
+                    'date': news.css('span.news-listing__item-time::text').get().strip(),
+                    'link': news.css('a::attr(href)').get()
+                }
+            except:
+                pass
+
+        next_page = response.css('li.news-listing__pagination-item.news-listing__pagination-item_active + li a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(f"https://www.mk.ru{next_page}", callback=self.parse)
