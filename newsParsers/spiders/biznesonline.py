f8adfe5eaeba73f415ead980360deeeeec7a81b9
vasilewvv <vasilewvv@gmail.com>
2021-01-27 19:22:49 +0300

    crawl news

--- newsParsers/spiders/biznesonline.py
+++ newsParsers/spiders/biznesonline.py
@@ -4,7 +4,19 @@ import scrapy
 class BiznesonlineSpider(scrapy.Spider):
     name = 'biznesonline'
     allowed_domains = ['business-gazeta.ru']
-    start_urls = ['http://business-gazeta.ru/']
+    start_urls = ['https://www.business-gazeta.ru/category/6']
 
     def parse(self, response):
-        pass
+        for news in response.css('article'):
+            try:
+                yield {
+                    'title': news.css('a.article-news__title::text').get().strip(),
+                    'comments': news.css('span.comments-num::text').get().strip(),
+                    'link': f"https://www.business-gazeta.ru{news.css('a.article-news__title::attr(href)').get()}"
+                }
+            except:
+                pass
+
+        next_page = f"https://www.business-gazeta.ru{response.css('a.person-page__more.js-pager__more::attr(href)').get()}"
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
